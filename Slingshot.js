b5b13982219aa40985b2e7d433e63b6c959630d3
Adiel1274 <78488019+Adiel1274@users.noreply.github.com>
2021-04-28 19:40:35 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -29,7 +29,7 @@ class SlingShot{
             stroke(48,22,8);
             if(pointA.x < 220) {
                 strokeWeight(4);
-                stroke(rgb(48,22,8));
+            
                 line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
                 line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
                 image(this.sling3,pointA.x -30, pointA.y -10,15,30);
